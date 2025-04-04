bc7add932ee2e4456fbd311e27e59f2c080e38da
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2025-04-04 20:44:36 +0200

    Fix typo

--- src/vec2.cpp
+++ src/vec2.cpp
@@ -1,17 +1,17 @@
 #include "vec2.hpp"
 #include <cmath>
 
-vec2::vec2() : x(0), y(0) { }
+vec2::vec2() : x(0), y(0) {}
 
-vec2::vec2(float _x, float _y) : x(_x), y(_y) { }
+vec2::vec2(float _x, float _y) : x(_x), y(_y) {}
 
-vec2::vec2(float value) : x(value), y(value) { }
+vec2::vec2(float value) : x(value), y(value) {}
 
-vec2::vec2(const vec2 &vec) : x(vec.x), y(vec.y) { }
+vec2::vec2(const vec2 &vec) : x(vec.x), y(vec.y) {}
 
-vec2::vec2(const vec2 &&vec) : x(vec.x), y(vec.y) { }
+vec2::vec2(const vec2 &&vec) : x(vec.x), y(vec.y) {}
 
-vec2::vec2(int _x, int _y) : x((float)_x), y((float)_y) { }
+vec2::vec2(int _x, int _y) : x((float)_x), y((float)_y) {}
 
 void vec2::operator=(const vec2 &vec)
 {
@@ -19,7 +19,7 @@ void vec2::operator=(const vec2 &vec)
     y = vec.y;
 }
 
-void vec2::operator=(const vec2&& vec)
+void vec2::operator=(const vec2 &&vec)
 {
     x = vec.x;
     y = vec.y;
@@ -40,7 +40,7 @@ vec2 vec2::operator+(const float value)
     return vec2(x + value, y + value);
 }
 
-vec2 vec2::operator-(const vec2& vec)
+vec2 vec2::operator-(const vec2 &vec)
 {
     return vec2(x - vec.x, y - vec.y);
 }
@@ -77,10 +77,10 @@ vec2 vec2::operator/(const int value)
 
 float vec2::dot(const vec2 &left, const vec2 &right)
 {
-    float leftLengt = sqrt(left.x * left.x + left.y * left.y);
+    float leftLength = sqrt(left.x * left.x + left.y * left.y);
     float rightLength = sqrt(right.x * right.x + right.y * right.y);
 
-    vec2 unitLeft = left / leftLengt;
+    vec2 unitLeft = left / leftLength;
     vec2 unitRight = right / rightLength;
 
     return unitLeft.x * unitRight.x + unitLeft.y * unitRight.y;
--- src/vec2.hpp
+++ src/vec2.hpp
@@ -3,41 +3,38 @@
 
 struct vec2
 {
-    float x,y;
-    
+    float x, y;
+
     vec2();
     vec2(float _x, float _y);
     vec2(float value);
-    vec2(const vec2& vec);
-    vec2(const vec2&& vec);
+    vec2(const vec2 &vec);
+    vec2(const vec2 &&vec);
     vec2(int _x, int _y);
-    
-    void operator=(const vec2& vec);
-    void operator=(const vec2&& vec);
+
+    void operator=(const vec2 &vec);
+    void operator=(const vec2 &&vec);
     void operator=(const int value) = delete;
-    void operator=(const float valule) = delete;
-    
-    
-    vec2 operator+(const vec2& vec);
+    void operator=(const float value) = delete;
+
+    vec2 operator+(const vec2 &vec);
     vec2 operator+(const int value);
     vec2 operator+(const float value);
-    
-    vec2 operator-(const vec2& vec);
+
+    vec2 operator-(const vec2 &vec);
     vec2 operator-(const int value);
     vec2 operator-(const float value);
-    
-    vec2 operator*(const vec2& vec) = delete;
+
+    vec2 operator*(const vec2 &vec) = delete;
     vec2 operator*(const int value);
     vec2 operator*(const float value);
-    
-    vec2 operator/(const vec2& vec) = delete;
+
+    vec2 operator/(const vec2 &vec) = delete;
     vec2 operator/(float value);
     vec2 operator/(const int value);
-    friend vec2 operator/(const vec2& vec, float value);
-    
-    static float dot(const vec2& left, const vec2& right);
+    friend vec2 operator/(const vec2 &vec, float value);
+
+    static float dot(const vec2 &left, const vec2 &right);
 };
-
-
 
 #endif // !VEC2_H
